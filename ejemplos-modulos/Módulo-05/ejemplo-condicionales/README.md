000c432740ceddcb424030513d61ac4ad1413ada
Rocio Cano <rociocgn@gmail.com>
2026-01-09 13:10:53 +0100

    Añadidos ejemplos del módulo 5

---
+++ ejemplos-modulos/Módulo-05/ejemplo-condicionales/README.md
@@ -0,0 +1,12 @@
+Ejemplos vistos en las grabaciones del Modulo 5 - Sandbox de JavaScript
+
+- Conceptos
+- Math.random
+- Layout del juego Adivinar
+- Handle botón
+- IsNaN
+- If básico
+- Separación Concerns
+- Switch
+- Ternarios
+- Pistas
---
+++ ejemplos-modulos/Módulo-05/ejemplo-condicionales/src/main.js
@@ -0,0 +1,70 @@
+const generarNumeroAleatorio = () => Math.floor(Math.random() * 100) + 1;
+
+const numeroParaAdivinar = generarNumeroAleatorio();
+
+const NO_ES_UN_NUMERO = 0;
+const EL_NUMERO_ES_MAYOR= 1;
+const EL_NUMERO_ES_MENOR=2;
+const ES_EL_NUMERO_SECRETO = 3;
+
+const muestraMensajeComprobacion = (texto, estado) => {
+  let mensaje = '';
+
+switch (estado) {
+    case NO_ES_UN_NUMERO:
+        mensaje = `${texto} no es un número válido.`; 
+        break;
+    case EL_NUMERO_ES_MAYOR:
+        mensaje = `Uy! El número ${texto} es mayor que el número secreto. Inténtalo de nuevo.`; 
+        break;
+    case EL_NUMERO_ES_MENOR:
+        mensaje = `Uy! El número ${texto} es menor que el número secreto. Inténtalo de nuevo.`; 
+        break;
+    case ES_EL_NUMERO_SECRETO:
+        mensaje = `¡Felicidades! Has adivinado el número!!!🎉🎉🎉.`; 
+        break;
+    default:
+        mensaje = 'No se ha podido determinar el estado.';
+        break;
+  }
+  document.getElementById("resultado").innerHTML = mensaje;
+}
+
+const comprobarNumero = (texto) => {
+  const numero = parseInt(texto);
+  const esUnNumero = !isNaN(numero);
+
+ if(!esUnNumero){
+    return NO_ES_UN_NUMERO;
+ }
+
+if (numero === numeroParaAdivinar) {
+  return ES_EL_NUMERO_SECRETO;
+}
+
+    return numero > numeroParaAdivinar
+    ? EL_NUMERO_ES_MAYOR
+    : EL_NUMERO_ES_MENOR;
+  };
+
+  /* Primero me das un número y comprobamos que sea un número. Si no lo es, devolvemos NO_ES_UN_NUMERO. 
+  A continuación, comparamos el número con el número para adivinar y devolvemos el estado correspondiente. */
+
+const handleCompruebaClick = () => {
+    const texto = document.getElementById('numero').value;
+    const estado = comprobarNumero(texto);
+    muestraMensajeComprobacion(texto, estado);
+}
+
+
+const botonComprobar = document.getElementById('comprobar');
+botonComprobar.addEventListener('click', handleCompruebaClick);
+
+  /* if (numero === numeroParaAdivinar) {
+      resultado = ES_EL_NUMERO_SECRETO;
+    } else {
+      resultado = NO_ES_EL_NUMERO_SECRETO;
+    }
+  } else {
+    resultado = NO_ES_UN_NUMERO;
+  }*/
