784b037d4f8ee57617838fa04f782dd504fca077
Rocio Cano <rociocgn@gmail.com>
2026-02-04 17:56:06 +0100

    Añadido correctamente el ejemplo del Módulo 7 Testing en TypeScript

--- ejemplos-modulos/Módulo-07/ejemplo-testing-ts/src/motor.ts
+++ ejemplos-modulos/Módulo-07/ejemplo-testing-ts/src/motor.ts
@@ -1,20 +1,30 @@
-import { partida, MAXIMO_INTENTOS, Estado } from './modelo.js';
+import { partida, MAXIMO_INTENTOS, Estado } from "./modelo";
 
 export const generarNumeroAleatorio = (): number => Math.floor(Math.random() * 101);
 
 const hasSuperadoMaximoIntentos = () : boolean => partida.intentos >= MAXIMO_INTENTOS;
 
-export const comprobarNumeroB = (texto : string, numeroParaAdivinar : number) => {
+export const comprobarNumeroB = (texto : string, numeroParaAdivinar : number) : Estado => {
+  const numero = parseInt(texto);
+  const esUnNumero = !isNaN(numero);
 
-  return EL_NUMERO_ES_MAYOR : Estado;
-}
+  if(!esUnNumero) {
+    return "NO_ES_UN_NUMERO";
+  } 
+
+  if(numero === numeroParaAdivinar) {
+    return "ES_EL_NUMERO_SECRETO";
+  }
+    
+  return numero < numeroParaAdivinar ? "EL_NUMERO_ES_MENOR" : "EL_NUMERO_ES_MAYOR";
+};
 
 
 export const comprobarNumero = (texto : string) => {
   const numero = parseInt(texto);
   const esUnNumero = !isNaN(numero);
 
- if(!esUnNumero){
+ if(!esUnNumero) {
     return "NO_ES_UN_NUMERO";
  }
 
