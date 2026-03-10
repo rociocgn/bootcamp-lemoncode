742819a7a2576271d636b6666245451a91fe287c
Rocio Cano <rociocgn@gmail.com>
2026-03-10 12:13:35 +0100

    Aádido el segundo ejemplo de Arrays con TypeScript

---
+++ ejemplos-modulos/Módulo-08/ejemplo-02-arrays-ts/src/main.ts
@@ -0,0 +1,9 @@
+const edades : number[] = [25, 30, 35, 40, 45];
+
+const peliculas : string[] = ["Inception", "The Matrix", "Interstellar", "The Dark Knight"];
+
+const pintaEdades = (edadColeccion : number[]) => {
+    console.groupCollapsed(edadColeccion);
+}
+
+pintaEdades(edades);
